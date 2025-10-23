512fa8002464097e24ccd458f64d41b89417b7d7
orange-dinosaur <user@randomprojects.xyz>
2025-10-24 01:26:13 +0200

    Create dynamic route to display documents

---
+++ src/app/(protected)/document/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default async function DocumentId({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const documentId = (await params).id;
+
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <div>DOCUMENT: {documentId}</div>
+        </div>
+    );
+}
