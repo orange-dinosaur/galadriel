0264c03ac8e343b54c33a7aa4efc4367119d6c14
orange-dinosaur <user@randomprojects.xyz>
2025-10-23 23:19:51 +0200

    Update registration and login components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,11 @@
-export default function Login() {
-    return <div>LOGIN</div>;
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
+  return (
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
