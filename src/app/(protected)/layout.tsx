b05546eb4f356c2f069e3828e5cd4655377b59b2
orange-dinosaur <user@randomprojects.xyz>
2025-10-25 03:36:55 +0200

    Add layout for all protected routes

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,50 @@
+import { user } from '@/auth/user';
+import { AppSidebar } from '@/components/app-sidebar';
+import { SiteHeader } from '@/components/site-header';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import axiosInstance from '@/lib/axiosInstance';
+import { AppSidebarData, DbDocumentRow } from '@/lib/custom-types';
+
+export const iframeHeight = '800px';
+
+export default async function ProtectedLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const u = await user.getUser();
+
+    const response = await axiosInstance(
+        'http://localhost:3000/api/documents',
+        'get'
+    );
+    const d = response.data.documents;
+    const documents = DbDocumentRow.fromApiResponse(d);
+
+    const data = AppSidebarData.fromUserAndDocuments(
+        {
+            name: u.name,
+            email: u.email,
+            avatar: process.env.NEXT_PUBLIC_AVATAR_ENDPOINT + u.name,
+        },
+        documents
+    );
+
+    return (
+        <div className="[--header-height:calc(--spacing(14))]">
+            <SidebarProvider className="flex flex-col">
+                <SiteHeader />
+                <div className="flex flex-1">
+                    <AppSidebar data={data.toObject()} />
+                    <SidebarInset>
+                        <div className="flex flex-1 flex-col gap-4 p-4">
+                            <div className="min-h-screen flex-1 md:min-h-min">
+                                {children}
+                            </div>
+                        </div>
+                    </SidebarInset>
+                </div>
+            </SidebarProvider>
+        </div>
+    );
+}
