05aec9677c1d7a954a554049b9ebf4303ce40eb4
orange-dinosaur <user@randomprojects.xyz>
2025-10-25 04:00:32 +0200

    Update documents api to filter projects through params

--- src/app/(protected)/project/[id]/page.tsx
+++ src/app/(protected)/project/[id]/page.tsx
@@ -1,13 +1,32 @@
-export default async function DocumentId({
+import axiosInstance from '@/lib/axiosInstance';
+import { DbDocumentRow } from '@/lib/custom-types';
+
+export default async function ProjectId({
     params,
 }: {
     params: Promise<{ id: string }>;
 }) {
-    const documentId = (await params).id;
+    const projectId = (await params).id;
+
+    const response = await axiosInstance(
+        `http://localhost:3000/api/documents?projectId=${projectId}`,
+        'get'
+    );
+
+    const d = response.data.documents;
+    const documents = DbDocumentRow.fromApiResponse(d);
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            <div>PROJECT: {documentId}</div>
+        <div>
+            <div>DOCUMENTS OF PROJECT {projectId}</div>
+            <br />
+            <div>
+                {documents.map((doc: any) => (
+                    <a href={`/document/${doc.$id}`} key={doc.$id}>
+                        <div key={doc.$id}>{doc.$id}</div>
+                    </a>
+                ))}
+            </div>
         </div>
     );
 }
--- src/app/api/documents/route.ts
+++ src/app/api/documents/route.ts
@@ -2,21 +2,31 @@ import { createSessionClient } from '@/appwrite/config';
 import { user } from '@/auth/user';
 import { DbDocumentRow } from '@/lib/custom-types';
 import { cookies } from 'next/headers';
+import { NextRequest } from 'next/server';
 import { Query } from 'node-appwrite';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     try {
+        const searchParams = request.nextUrl.searchParams;
+        const project = searchParams.get('projectId'); // e.g. `/api/documents?projectId=hello`
+
         user.sessionCookie = (await cookies()).get('session');
 
         const { authenticatedUser, database } = await user.getUserAndDb();
 
+        const queries: string[] = [
+            Query.equal('userId', authenticatedUser.$id),
+        ];
+        if (project) {
+            queries.push(Query.equal('projectId', project));
+        } else {
+            queries.push(Query.orderAsc('projectId'));
+        }
+
         const rows = await database.listRows({
             databaseId: process.env.NEXT_PUBLIC_DATABASE_ID || '',
             tableId: process.env.NEXT_PUBLIC_COLLECTION_DOCUMENTS || '',
-            queries: [
-                Query.equal('userId', authenticatedUser.$id),
-                Query.orderAsc('projectId'),
-            ],
+            queries: queries,
         });
 
         const documents = DbDocumentRow.fromApiResponse(rows.rows);
