fbab72c84174aff64f3fbd4afab2838dc75ca4cd
orange-dinosaur <user@randomprojects.xyz>
2025-11-09 02:04:02 +0100

    Add images trusted hostname

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME_1 || '',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
