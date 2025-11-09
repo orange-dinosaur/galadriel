da3681449859a9f2f758b396fcf89fb8e71ff9e2
orange-dinosaur <user@randomprojects.xyz>
2025-11-09 13:37:37 +0100

    Update middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,5 +28,14 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/', '/home', '/login', '/register'],
+    matcher: [
+        /*
+         *  Match all request paths except for the ones starting with:
+         *  - api (API routes)
+         *  - _next/static (static files)
+         *  - _next/image (image optimization files)
+         *  - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
 };
